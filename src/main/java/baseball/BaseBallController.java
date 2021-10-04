35dd81c7ead2c14c95ef57b0295c4d5fcd73eabd
ChangSubKwak <paradise1732@gmail.com>
2021-10-05 01:54:16 +0900

    feat: 게임을 구동하는 보조 클래스(BaseBallController)의 구현

---
+++ src/main/java/baseball/BaseBallController.java
@@ -0,0 +1,26 @@
+package baseball;
+
+import baseball.domain.BallJudge;
+import baseball.domain.BallJudgeResult;
+import baseball.domain.BallNumbers;
+import baseball.view.InputView;
+import baseball.view.ResultView;
+
+public class BaseBallController {
+    public static void innerLoop(BallJudge ballJudge, BallNumbers computerBallNumbers) {
+        while (!ballJudge.isGameEnd()) {
+            tryAndCatch(ballJudge, computerBallNumbers);
+        }
+    }
+
+    private static void tryAndCatch(BallJudge ballJudge, BallNumbers computerBallNumbers) {
+        InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
+        try {
+            BallJudgeResult ballJudgeResult = ballJudge.judge(computerBallNumbers, BallNumbers.from(inputView.inputBallNumbers()));
+            resultView.outputResult(ballJudgeResult.strikeCount(), ballJudgeResult.ballCount());
+        } catch (IllegalArgumentException e) {
+            resultView.outputErrorMessage(e.getMessage());
+        }
+    }
+}
