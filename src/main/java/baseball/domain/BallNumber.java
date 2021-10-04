3971cc2f36eeac641cb3c05a58b379166427ecba
ChangSubKwak <paradise1732@gmail.com>
2021-10-05 01:45:06 +0900

    feat: 1에서 9를 표현하는 숫자(BallNumber) 클래스 구현

---
+++ src/main/java/baseball/domain/BallNumber.java
@@ -0,0 +1,36 @@
+package baseball.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BallNumber {
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 9;
+    private static final String BALL_NUMBER_OUT_OF_RANGE_EXCEPTION_STATEMENT = "볼 넘버가 범위를 벗어났습니다.";
+    private static final Map<Integer, BallNumber> ballNumberMap;
+
+    static {
+        ballNumberMap = new HashMap<>();
+        for (int i = MIN_VALUE; i < MAX_VALUE; i++) {
+            ballNumberMap.put(i, new BallNumber(i));
+        }
+    }
+
+    final int ballNumber;
+
+    private BallNumber(int ballNumber) {
+        validate(ballNumber);
+        this.ballNumber = ballNumber;
+    }
+
+    public static BallNumber from(int ballNumber) {
+        validate(ballNumber);
+        return ballNumberMap.get(ballNumber);
+    }
+
+    private static void validate(int ballNumber) {
+        if (ballNumber < BallNumber.MIN_VALUE || ballNumber > BallNumber.MAX_VALUE) {
+            throw new IllegalArgumentException(BALL_NUMBER_OUT_OF_RANGE_EXCEPTION_STATEMENT);
+        }
+    }
+}
