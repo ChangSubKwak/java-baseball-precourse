a65071021c440cbebe034bdf169409863d7e58e2
ChangSubKwak <paradise1732@gmail.com>
2021-10-05 01:49:15 +0900

    feat: 3자리 수의 랜덤 생성을 담당하는 클래스(BallCreator) 구현

---
+++ src/main/java/baseball/domain/BallCreator.java
@@ -0,0 +1,28 @@
+package baseball.domain;
+
+import static baseball.domain.BallNumber.*;
+import static baseball.domain.BallNumbers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nextstep.utils.Randoms;
+
+public class BallCreator {
+    public static BallNumbers create() {
+        List<Integer> ballNumbersList = new ArrayList<>();
+
+        while(ballNumbersList.size() < BALL_NUMBERS_SIZE) {
+            addRandomBallNumber(ballNumbersList);
+        }
+        return BallNumbers.from(ballNumbersList);
+    }
+
+    private static void addRandomBallNumber(List<Integer> ballNumbersList) {
+        int value = Randoms.pickNumberInRange(MIN_VALUE, MAX_VALUE);
+        if (ballNumbersList.contains(value)) {
+            return;
+        }
+        ballNumbersList.add(value);
+    }
+}
